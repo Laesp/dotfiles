0bc2d2a3d1f9e9950d8a0e9e409edc8ef4b2af7d
Laesp <treyhackshaw@gmail.com>
2020-06-21 04:14:47 -0400

    Added a copy and commit dotfiles script

---
+++ cpdotfiles.sh
@@ -0,0 +1,40 @@
+cpFunc () {
+	cp -i -r -v ~/.i3/ ~/github/dotfiles/
+	cp -i -r -v ~/.i3status.conf ~/github/dotfiles/
+	cp -i -r -v ~/.bashrc ~/github/dotfiles
+	cp -i -r -v ~/programming/bashscripts/cpdotfiles.sh ~/github/dotfiles;} # Recursion lol
+
+
+
+
+commitFuncQuestion () {
+    while true; do
+    read -p "Would you also like to commit your dotfiles? " yn
+    case $yn in
+        [Yy]* ) commitFuncCommand;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+}
+
+commitFuncCommand () {
+	cd ~/github/dotfiles
+	git add .
+   	read -p "Please enter your commit message: " commitMessage
+   	echo Is $commitMessage your commit message?
+	git commit -m "$commitMessage" ~/github/dotfiles &&
+	sleep 2
+	git push origin master;
+}
+
+
+while true; do
+    read -p "Are you sure you want to update your dotfiles copy? " yn
+    case $yn in
+        [Yy]* ) cpFunc; commitFuncQuestion; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
